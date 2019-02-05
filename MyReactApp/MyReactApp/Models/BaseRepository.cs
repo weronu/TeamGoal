7a1985c0431dbeb5607e27728b114ec790a07a97
Veronika Uhrova <weron.u@gmail.com>
2019-02-05 16:51:09 +0100

    base and patient repository

---
+++ MyReactApp/MyReactApp/Models/BaseRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace MyReactApp.Models
+{
+    public class BaseRepository<T> where T : class 
+    {
+        internal readonly LocalDatabaseContext myContext = new LocalDatabaseContext();
+
+        public IEnumerable<T> GetAll()
+        {
+            return myContext.Set<T>();
+        }
+
+    }
+}
---
+++ MyReactApp/MyReactApp/Models/PatientRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace MyReactApp.Models
+{
+    public class PatientRepository : BaseRepository<Patient>
+    {
+        public Patient GetOne(int id)
+        {
+            return myContext.Set<Patient>().First(x => x.PatientId == id);
+        }
+    }
+}
